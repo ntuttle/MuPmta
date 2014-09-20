5e59d86be9027efc73eff970843b449b42313e1f
ntuttle <ntuttle@efflux.com>
2014-09-19 17:10:03 -0700

    .

--- apps/current_queues/index.php
+++ apps/current_queues/index.php
@@ -2,7 +2,8 @@
 define('output','html');
 require_once __DIR__.'/../../core/core.php';
 Req('class/current_queues.class.php',__DIR__);
-www::ScriptHead('Current Queues');
 $CQ = new CurrentQueues($CFG->DB);
+
+echo www::ScriptHead('Current Queues');
 echo www::Alt(Debug($CQ));
 ?>
