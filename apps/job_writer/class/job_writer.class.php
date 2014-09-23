06b33febe416c6e0bf37753236ff8634f34e5c42
ntuttle <ntuttle@efflux.com>
2014-09-23 12:53:16 -0700

    .

--- apps/job_writer/class/job_writer.class.php
+++ apps/job_writer/class/job_writer.class.php
@@ -7,6 +7,23 @@ class job_writer {
   public function __construct($CFG)
     {
       $this->DB = $CFG->DB;
+      $this->CheckLineup();
+    }
+  public function CheckLineup()
+    {
+      $Q = $this->DB->GET('MUP.jobs.lineup__'.strtoupper(hostname),['status'=>'PENDING','send_date__<='=>'NOW()','active'=>1],['id','listID','json_params'],10);
+      if(!empty($Q)){
+        $Q = isset($Q['id'])?[$Q['id']=>$Q]:$Q;
+        foreach($Q as $i=>$q){
+          $IDs[] = [$i];
+          $this->Lineup[$i] = $q;
+        }
+        $this->BuildJobs();
+      }
+    }
+  public function BuildJobs()
+    {
+      
     }
 }
 ?>
