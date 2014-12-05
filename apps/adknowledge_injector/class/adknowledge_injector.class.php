1aed2c3454856c5611600c516f70876b0b4481c1
ntuttle <ntuttle@efflux.com>
2014-12-05 11:48:58 -0800

    .

--- apps/adknowledge_injector/class/adknowledge_injector.class.php
+++ apps/adknowledge_injector/class/adknowledge_injector.class.php
@@ -43,19 +43,19 @@ class adknowledge_injector {
                         $domain    => 'domain']];
     }
   public function array_to_xml($info, &$xml_info) {
-    foreach($student_info as $key => $value) {
+    foreach($info as $key => $value) {
         if(is_array($value)) {
             if(!is_numeric($key)){
-                $subnode = $xml_student_info->addChild("$key");
+                $subnode = $xml_info->addChild("$key");
                 array_to_xml($value, $subnode);
             }
             else{
-                $subnode = $xml_student_info->addChild("item$key");
+                $subnode = $xml_info->addChild("item$key");
                 array_to_xml($value, $subnode);
             }
         }
         else {
-            $xml_student_info->addChild("$key",htmlspecialchars("$value"));
+            $xml_info->addChild("$key",htmlspecialchars("$value"));
         }
     }
 }
