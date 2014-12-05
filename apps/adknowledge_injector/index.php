7d1aca76f304c4c9dc7d465e3c8d7ae0be4b79df
ntuttle <ntuttle@efflux.com>
2014-12-05 11:07:04 -0800

    .

--- apps/adknowledge_injector/index.php
+++ apps/adknowledge_injector/index.php
@@ -1,7 +1,7 @@
 <?php
   define('output','html');
   require_once __DIR__.'/../../core/core.php';
-  Req('config_writer/class/adknowledge_injector.class.php',APPS);
+  Req('adknowledge_injector/class/adknowledge_injector.class.php',APPS);
   echo www::ScriptHead('Adknowledge Injector');
   $INJECT = new adknowledge_injector($CFG->DB);
   
